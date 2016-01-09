65092b8df18ea8ee569a1e63029b853b57079ef9
Roozbeh Daneshvar <roozbehdaneshvar@MacBook-Air.home>
2016-01-09 00:34:24 -0500

    add fft

--- discrete-eoc.py
+++ discrete-eoc.py
@@ -5,6 +5,10 @@ __email__ = "roozbeh.daneshvar@gmail.com"
 
 import random
 import math as math
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
 
 # -----------------------------
 
@@ -71,9 +75,6 @@ def iterations(steps = 1000, initial_x = 0.38, initial_a = 3.8, r = 0.005):
 # -----------------------------
 
 def plot_parameters(inputs):
-    import matplotlib
-    import matplotlib.pyplot as plt
-
     ax = plt.gca()
     ax.ticklabel_format(useOffset=False)
     for a in inputs:
@@ -92,9 +93,50 @@ randomness = 0.00005
 a = []
 #initial_as = [0.3, 0.7, 1.0, 1.4]
 initial_as = [1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
-for ia in initial_as:
-    a.append(iterations(steps = n, initial_x = 0.38, initial_a = ia, r = randomness))
+#for ia in initial_as:
+#    a.append(iterations(steps = n, initial_x = 0.38, initial_a = ia, r = randomness))
 
-plot_parameters(a)
+#plot_parameters(a)
+
+#print np.fft.fft(np.exp(2j * np.pi * np.arange(8) / 8))
+
+
+
+#t = np.arange(256)
+#sp = np.fft.fft(np.sin(t))
+#freq = np.fft.fftfreq(t.shape[-1])
+#plt.plot(freq, sp.real, freq, sp.imag)
+#plt.show()
+
+
+
+
+Fs = 150.0;  # sampling rate
+Ts = 1.0/Fs; # sampling interval
+t = np.arange(0, 1, Ts) # time vector
+
+print 't: ', t
+
+ff = 5;   # frequency of the signal
+y = np.sin(2 * np.pi * ff * t) + np.sin(2 * np.pi * 9 * t)
+
+#y = [0, 1, 2, 1, 0, -1, -2, -1]
+y = iterations(steps = len(t), initial_x = 0.38, initial_a = 1.7, r = 0)
+print 'y: ', y
+
+n = len(y) # length of the signal
+k = np.arange(n)
+T = n/Fs
+frq = k/T # two sides frequency range
+frq = frq[range(n/2)] # one side frequency range
+
+Y = np.fft.fft(y)/n # fft computing and normalization
+Y = Y[range(n/2)]
+
+print 'frq: ', frq
+print 'Y: ', Y
+print 'abs(Y): ', abs(Y)
+
+plot_parameters([abs(Y)])
 
 # -----------------------------
