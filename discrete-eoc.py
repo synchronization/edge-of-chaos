71f0433b1b839ef251bbf76ead35be1761fd3391
Roozbeh Daneshvar <roozbehdaneshvar@MacBook-Air.home>
2016-01-12 22:42:27 -0500

    cleaning and one reference

--- discrete-eoc.py
+++ discrete-eoc.py
@@ -36,7 +36,7 @@ def dynamics_function(x, a):
 
 # -----------------------------
 
-def create_sequence(ic, a, n):
+def create_sequence(ic=0.5, a=1.5, n=10):
     """
     ic: initial condition (number to start with)
     a: parameter
@@ -55,10 +55,9 @@ def create_sequence(ic, a, n):
 
 # -----------------------------
 
-def iterations(steps = 1000, initial_x = 0.38, initial_a = 3.8, r = 0.005):
+def iterations(steps = 1000, initial_x = 0.38, initial_a = 3.8, r = 0.005, min_x = 0.0, max_x = 1.0, min_a = 0, max_a = 2.0):
     x_ = initial_x
     x = []
-
     a_ = initial_a
     a = [a_]
 
@@ -66,7 +65,6 @@ def iterations(steps = 1000, initial_x = 0.38, initial_a = 3.8, r = 0.005):
         f = x_
     
         for k in range(1, 33):
-#            x_ = a[n] * x_ * (1.0 - x_)
             x_ = dynamics_function(x_, a[n])
         
         x_ = x_ + (random.random() - 0.5) * r
@@ -83,19 +81,17 @@ def iterations(steps = 1000, initial_x = 0.38, initial_a = 3.8, r = 0.005):
     
         if a[n+1] < 0:
             a[n+1] = 0.0
-        
         if a[n+1] > 2:
             a[n+1] = 2.0
     
         for k in range(1, 32):
-#            x_ = a[n] * x_ * (1.0 - x_)
             x_ = dynamics_function(x_, a[n])
 
     return a
 
 # -----------------------------
 
-def plot_parameters(inputs):
+def plot_time_series(inputs):
     ax = plt.gca()
     ax.ticklabel_format(useOffset=False)
     for a in inputs:
@@ -105,70 +101,70 @@ def plot_parameters(inputs):
 
 # -----------------------------
 
-# transient
-result = create_sequence(0.5, 3.2, 100)
-print 'transient result: ', result
-# actual
-result = create_sequence(result[-1], 3.2, 100)
-print 'actual result: ', result
+def frequency_spectrum():
+    n = 16000
+    randomness = 0.00005
 
-#sys.exit(0)
+    a = []
 
-#a1 = iterations(steps = 1000, r = 0.0)
-#a2 = iterations(steps = 1000, r = 0.005)
+    #print np.fft.fft(np.exp(2j * np.pi * np.arange(8) / 8))
 
-n = 16000
-randomness = 0.00005
+    #t = np.arange(256)
+    #sp = np.fft.fft(np.sin(t))
+    #freq = np.fft.fftfreq(t.shape[-1])
+    #plt.plot(freq, sp.real, freq, sp.imag)
+    #plt.show()
 
-a = []
-#initial_as = [0.3, 0.7, 1.0, 1.4]
-initial_as = [1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
-#for ia in initial_as:
-#    a.append(iterations(steps = n, initial_x = 0.38, initial_a = ia, r = randomness))
+    Fs = 150.0;  # sampling rate
+    Ts = 1.0/Fs; # sampling interval
+    t = np.arange(0, 1, Ts) # time vector
 
-#plot_parameters(a)
+    print 't: ', t
 
-#print np.fft.fft(np.exp(2j * np.pi * np.arange(8) / 8))
+    ff = 5;   # frequency of the signal
+    y = np.sin(2 * np.pi * ff * t) + np.sin(2 * np.pi * 9 * t)
 
+    #y = [0, 1, 2, 1, 0, -1, -2, -1]
+    #y = iterations(steps = len(t), initial_x = 0.38, initial_a = 1.7, r = 0)
+    result = create_sequence(ic=result[-1], a=3.2, n=len(t))
+    y = result
+    print 'y: ', y
 
+    n = len(y) # length of the signal
+    k = np.arange(n)
+    T = n/Fs
+    frq = k/T # two sides frequency range
+    frq = frq[range(n/2)] # one side frequency range
 
-#t = np.arange(256)
-#sp = np.fft.fft(np.sin(t))
-#freq = np.fft.fftfreq(t.shape[-1])
-#plt.plot(freq, sp.real, freq, sp.imag)
-#plt.show()
+    Y = np.fft.fft(y)/n # fft computing and normalization
+    Y = Y[range(n/2)]
 
+    print 'frq: ', frq
+    print 'Y: ', Y
+    print 'abs(Y): ', abs(Y)
 
+    plot_time_series([abs(Y)])
 
+# -----------------------------
 
-Fs = 150.0;  # sampling rate
-Ts = 1.0/Fs; # sampling interval
-t = np.arange(0, 1, Ts) # time vector
+if __name__ == "__main__":
+    # transient
+    result = create_sequence(ic=0.5, a=3.2, n=100)
+    print 'transient result: ', result
+    # actual
+    result = create_sequence(ic=result[-1], a=3.2, n=100)
+    print 'actual result: ', result
 
-print 't: ', t
+    #sys.exit(0)
 
-ff = 5;   # frequency of the signal
-y = np.sin(2 * np.pi * ff * t) + np.sin(2 * np.pi * 9 * t)
+    #a1 = iterations(steps = 1000, r = 0.0)
+    #a2 = iterations(steps = 1000, r = 0.005)
 
-#y = [0, 1, 2, 1, 0, -1, -2, -1]
-#y = iterations(steps = len(t), initial_x = 0.38, initial_a = 1.7, r = 0)
-result = create_sequence(result[-1], 3.2, len(t))
-y = result
-print 'y: ', y
+    #initial_as = [0.3, 0.7, 1.0, 1.4]
+    #initial_as = [1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
+    #for ia in initial_as:
+    #    a.append(iterations(steps = n, initial_x = 0.38, initial_a = ia, r = randomness))
 
-n = len(y) # length of the signal
-k = np.arange(n)
-T = n/Fs
-frq = k/T # two sides frequency range
-frq = frq[range(n/2)] # one side frequency range
-
-Y = np.fft.fft(y)/n # fft computing and normalization
-Y = Y[range(n/2)]
-
-print 'frq: ', frq
-print 'Y: ', Y
-print 'abs(Y): ', abs(Y)
-
-plot_parameters([abs(Y)])
+    #plot_time_series(a)
 
 # -----------------------------
